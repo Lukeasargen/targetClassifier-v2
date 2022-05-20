02099668e40a40e9d3e49d827c1de4929d57c368
Luke A Sargen <lukeasargen@gmail.com>
2022-05-20 18:16:02 -0400

    Add torchscript to pipeline

--- pipline.py
+++ pipline.py
@@ -3,9 +3,7 @@ import numpy as np
 import torch
 import torchvision.transforms as T
 
-from classify import ClassifyModel
-from detect import SegmentModel
-from util import quantize_to_int, pil_loader
+from util import quantize_to_int, pil_loader, get_device
 from util import color_options, shape_options, letter_options
 
 def crop_center(pil_img, crop_width, crop_height):
@@ -20,9 +18,9 @@ def detect_pipline(model, pil_image, thresh=0.5, expand_ratio=1, min_size=0):
     # TODO : get meta data, location, size
     w, h = pil_image.size
     cropped = crop_center(pil_image, quantize_to_int(w,16), quantize_to_int(h,16))
-    img_ten = T.ToTensor()(cropped).unsqueeze(0)
+    img_ten = T.ToTensor()(cropped).unsqueeze(0).to(get_device())
     with torch.no_grad():
-        preds = model(img_ten.to(model.device)).cpu()
+        preds = model(img_ten).cpu()
     preds = preds[0,0].numpy()  # Get the first channel from the first sample
     torch.cuda.empty_cache()
     # preds = cv2.erode(preds, None, iterations=1)
@@ -50,9 +48,9 @@ def detect_pipline(model, pil_image, thresh=0.5, expand_ratio=1, min_size=0):
 def classify_pipline(model, patch, img_size=32):
     resized = patch["thumbnail"].resize((img_size,img_size))
     # resized = T.GaussianBlur(3,1)(resized)
-    img_ten = T.ToTensor()(resized).unsqueeze(0)
+    img_ten = T.ToTensor()(resized).unsqueeze(0).to(get_device())
     with torch.no_grad():
-        preds = model(img_ten.to(model.device))
+        preds = model(img_ten)
     patch["has_target"] = float(preds["has_target"])
     patch["has_letter"] = float(preds["has_letter"])
     patch["angle"] = float(preds["angle"])
@@ -76,7 +74,6 @@ def test_pipline(folder_path, segment_model, classify_model):
         os.makedirs(out_folder)
 
     paths = [os.path.join(os.getcwd(), x) for x in os.scandir(folder_path)]
-    images = [pil_loader(path) for path in paths]
 
     # from util import load_backgrounds
     # from generator import TargetGenerator
@@ -94,8 +91,8 @@ def test_pipline(folder_path, segment_model, classify_model):
     # paths = [''.join(random.choice(string.ascii_lowercase) for i in range(5)) for i in range(count)]
     # images = [generator.gen_segment(fill_prob=fill_prob)[0] for i in range(count)]
 
-    for path, image in zip(paths, images):
-
+    for path in paths:
+        image = pil_loader(path)
         name, ext = os.path.splitext(os.path.basename(path))
         with torch.cuda.amp.autocast():
             t0 = time.time()
@@ -122,17 +119,10 @@ def test_pipline(folder_path, segment_model, classify_model):
             plt.close(fig)
 
 def main():
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-    import os
-    segment_path = r"logs\segment\version_7\checkpoints"
-    segment_path = segment_path+"/"+os.listdir(segment_path)[0]
-    print(segment_path)
-    segment_model = SegmentModel.load_from_checkpoint(segment_path, map_location=device).eval().to(device)
-
-    classify_model = ClassifyModel.load_from_checkpoint("classify34.ckpt", map_location=device).eval().to(device)
-
-    root = r'C:\Users\lukeasargen\projects\aerial_real/'  # image folder, aerial_backgrounds, aerial_cropped, aerial_real, aerial_validate
+    print(get_device())
+    classify_model = torch.jit.load("dev/classify.pt", map_location=get_device())
+    segment_model = torch.jit.load("dev/detect.pt", map_location=get_device())
+    root = r'C:\Users\lukeasargen\projects\aerial_real/'  # image folder, aerial_backgrounds, aerial_cropped, aerial_real
     test_pipline(root, segment_model, classify_model)
 
 if __name__ == "__main__":
